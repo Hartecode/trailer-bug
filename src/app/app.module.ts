61f374315534c59fc9d2e3484d2a4ae9266a772e
Sean Harte <scsharte@gmail.com>
2019-12-15 16:07:40 -0500

    set folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonComponent } from './shared/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/stories/1-Button.stories.ts
@@ -0,0 +1,55 @@
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { withA11y } from '@storybook/addon-a11y';
+import { storiesOf, addDecorator } from '@storybook/angular';
+
+import { Button } from '@storybook/angular/demo';
+
+addDecorator(withA11y);
+
+export default {
+  title: 'Button',
+};
+
+export const text = () => ({
+  component: Button,
+  props: {
+    text: 'Hello Button',
+  },
+});
+
+export const emoji = () => ({
+  component: Button,
+  props: {
+    text: '😀 😎 👍 💯',
+  },
+});
+
+emoji.story = {
+  parameters: { notes: 'My notes on a button with emojis' },
+};
+
+export const withSomeEmojiAndAction = () => ({
+  component: Button,
+  props: {
+    text: '😀 😎 👍 💯',
+    onClick: action('This was clicked OMG'),
+  },
+});
+
+withSomeEmojiAndAction.story = {
+  name: 'with some emoji and action',
+  parameters: { notes: 'My notes on a button with emojis' },
+};
+
+export const buttonWithLinkToAnotherStory = () => ({
+  component: Button,
+  props: {
+    text: 'Go to Welcome Story',
+    onClick: linkTo('Welcome'),
+  },
+});
+
+buttonWithLinkToAnotherStory.story = {
+  name: 'button with link to another story',
+};
