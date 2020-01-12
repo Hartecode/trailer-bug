c65b1690df57fcc093323e5c7f29430808f40672
Sean Harte <scsharte@gmail.com>
2020-01-11 22:01:55 -0500

    added smoother animation and fixed btn icon alignment

--- src/app/feature/landing/components/image-carousel/image-carousel.component.ts
+++ src/app/feature/landing/components/image-carousel/image-carousel.component.ts
@@ -10,13 +10,18 @@ import { trigger, transition, animate, style } from '@angular/animations';
     trigger('enterInOut', [
       transition(':enter', [
         style({
-          transform: 'translateX(100%)'
+          transform: 'translateX(100%)',
+          opacity: 0
         }),
-        animate('0.5s ease-in')
+        animate('0.5s ease-out', style({
+          transform: 'translateX(0)',
+          opacity: 1
+        }))
       ]),
       transition(':leave', [
-        animate('0.25s ease-out', style({
-          transform: 'translateX(-100%)'
+        animate('0.25s ease-in-out', style({
+          transform: 'translateX(-100%)',
+          opacity: 0
         }))
       ])
     ])
