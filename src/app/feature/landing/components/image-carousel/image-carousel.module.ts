0b96b36c1e41485b4c6160bed38246fc93f9e1ce
Sean Harte <scsharte@gmail.com>
2020-03-07 18:27:52 -0500

    added pan directive to component

--- src/app/feature/landing/components/image-carousel/image-carousel.module.ts
+++ src/app/feature/landing/components/image-carousel/image-carousel.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { PanModule } from 'src/app/shared/directives/pan/pan.module';
 import { ImageCarouselComponent } from './image-carousel.component';
 
 export const imageCarouselSchema: NgModule = {
   declarations: [ImageCarouselComponent],
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, PanModule],
   exports: [ImageCarouselComponent]
 };
 
